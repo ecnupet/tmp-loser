13cde10d35cf7fd3cb631e407b3c82bbe4a5504a
shanchao <chao.shan01@liulishuo.com>
2021-03-09 16:17:12 +0800

    update interface

--- store/readwriter/commit_history.go
+++ store/readwriter/commit_history.go
@@ -1,5 +1,13 @@
 package readwriter
 
-type AnswerHistoryReadWriter interface {
-	CorrectAndInsert(quizID uint64, userID uint64, )
+import "ecnu.space/tmp-loser/model"
+
+// CommitHistoryReadWriter 声明基于表commit_history提供的功能
+type CommitHistoryReadWriter interface {
+	// CorrectAndInsert 批改用户提交的回答并插入数据库
+	CorrectAndInsert(commit *model.CommitHistory) error
+	// GetCommitsByUserIDAndTestID 根据用户ID个测验ID提供所有历史提交记录
+	GetCommitsByUserIDAndTestID() model.CommitHistory
+	// GetCommitsByQuizID 获取指定题目ID的所有提交记录
+	GetCommitsByQuizID(quizID uint64) []model.Quiz
 }
--- store/readwriter/quiz.go
+++ store/readwriter/quiz.go
@@ -1,5 +1,14 @@
 package readwriter
 
+import "ecnu.space/tmp-loser/model"
+
+// QuizReadWriter 声明基于表quiz提供的功能
 type QuizReadWriter interface {
-	BatchInsert()
+	// BatchInsert 批量插入题目
+	BatchInsert(quizs []*model.Quiz) error
+	// Insert 单个插入题目
+	Insert(quiz *model.Quiz) error
+	// GetQuizByType根据类型获取所有的题目
+	GetQuizByType(t string) ([]model.Quiz, error)
+
 }
