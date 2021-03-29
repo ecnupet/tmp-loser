5ff71cb9cc172cf020995eb26ec96f225128990d
shanchao <chao.shan01@liulishuo.com>
2021-03-29 17:02:29 +0800

    初步实现批改试卷httphttp接口

--- model/commit_history.go
+++ model/commit_history.go
@@ -4,14 +4,14 @@ import "time"
 
 // 映射表 commit_history
 type CommitHistory struct {
-	HistoryID  uint64    `xorm:"Int(20) NOT NULL PK AUTOINCR 'history_id'"`
-	UserName   string    `xorm:"user_name"`
-	QuestionID uint64    `xorm:"question_id"`
-	Choose     string    `xorm:"choose"`
-	QuizID     uint64    `xorm:"quiz_id"`
-	Order      uint64    `xorm:"order"`
+	HistoryID  uint64 `json:"history_id" xorm:"Int(20) NOT NULL PK AUTOINCR 'history_id'"`
+	UserName   string `json:"user_name" xorm:"user_name"`
+	QuestionID uint64 `json:"question_id" xorm:"question_id"`
+	Choose     string `json:"choose" xorm:"choose"`
+	QuizID     uint64 `json:"quiz_id" xorm:"quiz_id"`
+	Order      uint64 `json:"order" xorm:"order"`
 	// 0: 错误, 1: 正确, 2: 未选择
-	Correct    uint64    `xorm:"correct"`
-	Spend      uint64    `xorm:"spend"`
-	CreatedAt  time.Time `xorm:"DateTime CREATED 'created_at'"`
+	Correct   uint64    `json:"correct" xorm:"correct"`
+	Spend     uint64    `json:"speed" xorm:"spend"`
+	CreatedAt time.Time `json:"created_at" xorm:"DateTime CREATED 'created_at'"`
 }
