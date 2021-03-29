aff3a738cc2e926e39e0c4460827d2c73eb289c0
shanchao <chao.shan01@liulishuo.com>
2021-03-29 13:48:31 +0800

    add var err handle

--- http/handler/api/insert_quiz.go
+++ http/handler/api/insert_quiz.go
@@ -1,2 +1,14 @@
 package api
 
+import (
+	"ecnu.space/tmp-loser/model"
+	"github.com/gin-gonic/gin"
+)
+
+func InsertQuiz(c *gin.Context){
+	qq := model.QuizQuestion{}
+	err := c.ShouldBind(&qq)
+	if err != nil {
+		
+	}
+}
--- utils/context.go
+++ utils/context.go
@@ -4,28 +4,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type State int
+type HttpState int
 type Detail string
 
 const (
 	// http state
-	OK            State = 200
-	Redirect      State = 302
-	Unknown       State = 404
-	BadRequest    State = 400
-	InternalError State = 500
+	OK            HttpState = 200
+	// 临时重定向
+	Redirect      HttpState = 302
+	Unknown       HttpState = 404
+	BadRequest    HttpState = 400
+	InternalError HttpState = 500
 
+	//返回状态描述， 同步 C# person-manage服务的http返回格式，具体还得问hcs
 	GetSuccess    Detail = "获取成功"
 	GetFailForDB  Detail = "获取失败，数据库错误"
 	GetFailForNum Detail = "获取失败，题目不足"
 	PostSuccess   Detail = "上传成功"
-	PostFail      Detail = "上传失败"
+	PostFail      Detail = "上传失败, 参数错误"
 	DeleteSuccess Detail = "删除成功"
 	DeleteFail    Detail = "删除失败"
 )
 
 type Response struct {
-	State  State       `json:"state"`
+	State  HttpState       `json:"state"`
 	Detail Detail      `json:"detail"`
 	Data   interface{} `json:"data"`
 }
@@ -51,5 +53,13 @@ func HandleGetSuccess(c *gin.Context, data interface{}) {
 		State: OK,
 		Detail: GetSuccess,
 		Data: data,
+	})
+}
+
+func HandlePostQuizQuestion(c *gin.Context){
+	c.JSON(int(BadRequest), Response{
+		State: BadRequest,
+		Detail: PostFail,
+		Data: "",
 	})
 }
