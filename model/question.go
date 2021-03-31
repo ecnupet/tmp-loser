69306a3d6ebaee6bbc2e354489b590c34a7124df
shanchao <chao.shan01@liulishuo.com>
2021-03-31 14:38:32 +0800

    add questionType

--- model/question.go
+++ model/question.go
@@ -2,6 +2,23 @@ package model
 
 import "time"
 
+type QuestionType int
+
+const (
+	// 传染病
+	InfectiousDisease QuestionType = 0
+	// 寄生虫病
+	ParasiticDisease QuestionType = 1
+	// 内科疾病
+	InternalDisease QuestionType = 2
+	// 外产科疾病
+	ObstetricDisease QuestionType = 3
+	// 宠物常用手术知识
+	SurgicalKnowledge QuestionType = 4
+	// 宠物免疫知识
+	PetImmunity QuestionType = 5
+)
+
 // 映射表 question
 type Question struct {
 	QuestionID  uint32            `xorm:"PK AUTOINCR 'question_id'"`
